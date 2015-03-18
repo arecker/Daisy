9f441c95e2f2b5e053cf2ca5592df5457e2a76b3
Alex Recker <alex@reckerfamily.com>
2015-03-17 19:51:01 -0500

    Init: Initialization tests

--- daisy.js
+++ daisy.js
@@ -1,3 +1,9 @@
 var daisy = (function(){
-	return function(){};
-});
+	var Computation = function(raw){
+		this._raw = raw + '';
+	};
+
+	return function(initValue){
+		return new Computation(initValue);
+	};
+}());
