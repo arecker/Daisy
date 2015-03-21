be8c54ed14cd0a8c263e56d27873a7de41b01cbb
Alex Recker <alex@reckerfamily.com>
2015-03-20 21:27:37 -0500

    Bug: was leaving specs.js out of the linter

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
-			all: ['src/daisy.js', 'test/tests.js'],
+			all: ['src/daisy.js', 'test/specs.js'],
 			options: {
 				curly: true,
 				eqeqeq: true,
