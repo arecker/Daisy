6cc81c9315d9c1569eb2bf9438dba13476115082
Alex Recker <alex@reckerfamily.com>
2015-04-02 23:57:59 -0500

    Finished converting tabs to spaces on other files

--- test/clobber.js
+++ test/clobber.js
@@ -1,7 +1,7 @@
 (function(){
-	// Script to clobber some native functions
-	// to exercise polyfill restoration
-	// Code coverage turns you into a monster
-	Array.isArray = undefined;
-	Array.prototype.reduce = undefined;
+    // Script to clobber some native functions
+    // to exercise polyfill restoration
+    // Code coverage turns you into a monster
+    Array.isArray = undefined;
+    Array.prototype.reduce = undefined;
 }());
