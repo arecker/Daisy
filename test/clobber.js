634596b22a042fcc5ecab3207530077d73128997
Alex Recker <alex@reckerfamily.com>
2015-04-06 07:20:09 -0500

    Deleted everything

--- test/clobber.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    // Script to clobber some native functions
-    // to exercise polyfill restoration
-    // Code coverage turns you into a monster
-    Array.isArray = undefined;
-    Array.prototype.reduce = undefined;
-}());
